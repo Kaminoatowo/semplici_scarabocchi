6aec3c8669e260d028fd1535c2dae432d872a8fe
Jacopo Cocomello <72672976+Kaminoatowo@users.noreply.github.com>
2023-12-02 18:07:56 +0100

    remove babel

--- dist/src/general_layout.js
+++ dist/src/general_layout.js
@@ -1,3 +1,4 @@
 document.body.className = "bg-leaves bg-cover bg-repeat-y text-white text-xs sm:text-base";
+
 let main = document.getElementById("main");
 main.className = "bg-primary/75 mx-auto px-4 sm:px-6 md:max-w-2xl md:px-4 lg:max-w-4xl lg:px-12 relative";
